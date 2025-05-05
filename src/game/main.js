fed6f3ac0536e939fb7197713f21a0e5480e3dd5
teacherIan <iansmalloy@gmail.com>
2025-05-05 11:58:34 +0800

    JoeDown2

--- src/game/main.js
+++ src/game/main.js
@@ -3,33 +3,61 @@ import { Game as MainGame } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
+import { SceneOne } from './scenes/SceneOne';
 import { AUTO, Game } from 'phaser';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config = {
-    type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+  type: AUTO,
+  width: 1024,
+  height: 768,
+  parent: 'game-container',
+  backgroundColor: '#028af8',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
     },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  },
+  scene: [Boot, Preloader, SceneOne, MainMenu, MainGame, GameOver],
 };
 
 const StartGame = (parent) => {
+  return new Game({ ...config, parent });
+};
 
-    return new Game({ ...config, parent });
+function resizeApp() {
+  //screen.orientation.lock('portrait');
+  //ScreenOrientation.lock('portrait');
+  // Width-height-ratio of game resolution
+  // Replace 360 with your game width, and replace 640 with your game height
+  let game_ratio = 1200 / 800;
 
+  // Make div full height of browser and keep the ratio of game resolution
+  let div = document.getElementById('phaser-app');
+  div.style.width = window.innerHeight + 'px';
+  div.style.height = window.innerHeight + 'px';
+  // * game_ratio
+  // Check if device DPI messes up the width-height-ratio
+  let canvas = document.getElementsByTagName('canvas')[0];
+
+  let dpi_w = parseInt(div.style.width) / canvas.width;
+  let dpi_h = parseInt(div.style.height) / canvas.height;
+
+  let height = window.innerHeight * (dpi_w / dpi_h);
+  let width = height * game_ratio;
+
+  // Scale canvas
+  canvas.style.width = width + 'px';
+  canvas.style.height = height + 'px';
 }
+
+window.addEventListener('resize', resizeApp);
 
 export default StartGame;
---
+++ src/game/scenes/SceneOne.js
@@ -0,0 +1,168 @@
+import { Scene } from 'phaser';
+
+export class SceneOne extends Scene {
+  constructor() {
+    super('sceneOne');
+    console.log(this);
+    this.score = 0;
+    this.ScoreText = '';
+    this.ianChange = false;
+    this.theNumber = 0;
+    this.theText =
+      'You are Joe.\nUse the arrow keys to move & Jump.\nGo talk to Ian.';
+    //this.versionText = "Version 0.84";
+    this.check = false;
+    this.screenTouch = false;
+    this.moveLeft = false;
+    //Ian talk
+    this.ianText = 'How are you Joe?';
+    this.ianTextControl = '';
+    //this.scene.scale.lockOrientation('portrait');
+  }
+
+  preload() {
+    this.theText = this.add.text(16, 16, this.theText, {
+      fontSize: '32px',
+      fill: '#000',
+    });
+    this.ianTextControl = this.add.text(150, 320, this.ianTextControl, {
+      fontSize: '20px',
+      fill: '#000',
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.clothes = this.physics.add.sprite(520, 200, 'suitCase');
+    this.clothes.scale = 0.5;
+    this.clothes.visible = false;
+
+    this.Ian = this.physics.add.sprite(450, 480, 'ian');
+    this.Ian.scale = 2.4;
+    this.Ian.body.offset.y = -1.5;
+
+    this.player = this.physics.add.sprite(100, 515, 'dude');
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds = true;
+
+    this.player.body.setSize(0.5);
+    this.player.scale = 2;
+
+    //touch screen
+    this.input.on(
+      'pointerdown',
+      function () {
+        //this.moveLeft = !this.moveLeft;
+        this.screenTouch = true;
+        this.player.setVelocityX(25);
+        this.player.anims.play('right', true);
+        this.player.flipX = true;
+        if (this.player.body.touching.down) {
+          this.player.setVelocityY(-100);
+        }
+      },
+      this
+    );
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 7 }),
+      frameRate: 30,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 0 }),
+      frameRate: 0,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 7 }),
+      frameRate: 30,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'ianTurn',
+      frames: this.anims.generateFrameNumbers('ianLeft', { start: 0, end: 0 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+
+    this.platforms = this.physics.add.staticGroup();
+
+    //platforms.create(200, 100, "platform")
+    //platforms.create(300, 200, "platform").setScale(0.5).refreshBody();
+    //platforms.create(400, 600, "platform").setScale(2).refreshBody();
+
+    //ground platform
+    this.platforms.create(100, 590, 'platform').refreshBody();
+    //Ian's platform
+    this.platforms.create(525, 570, 'platform').refreshBody();
+    this.platforms.create(1000, 520, 'platform').refreshBody();
+    this.platforms.create(1300, 400, 'platform').refreshBody();
+
+    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.Ian, this.platforms);
+    this.physics.add.collider(this.clothes, this.platforms);
+  }
+  update() {
+    if (this.screenTouch) {
+      this.player.setVelocityX(25);
+    }
+
+    if (this.player.x > 302) {
+      this.player.angle += 2;
+    }
+    if (this.player.y > 1500) {
+      this.scene.start('sceneTwo');
+    }
+    if (this.screenTouch === false) {
+      if (this.cursors.left.isDown) {
+        this.player.setVelocityX(-50);
+        this.player.anims.play('left', true);
+        this.player.flipX = false;
+      } else if (this.cursors.right.isDown) {
+        this.player.setVelocityX(50);
+        this.player.anims.play('right', true);
+        this.player.flipX = true;
+      } else {
+        this.player.setVelocityX(0);
+        this.player.anims.play('turn', true);
+      }
+      if (this.cursors.up.isDown && this.player.body.touching.down) {
+        this.player.setVelocityY(-100);
+      }
+    }
+
+    if (this.player.x > 200 && this.ianChange === false) {
+      this.Ian.anims.play('ianTurn', true);
+      this.Ian.y = 480;
+      this.ianChange = true;
+      this.Ian.body.offset.y = -2.6;
+      this.ianTextControl.setText(
+        `No clothes again, eh Joe?! \nJump up here. \nI've got some extra for ya.`
+      );
+      this.clothes.visible = true;
+    }
+    if (this.cursors.space.isDown) {
+      this.theNumber = this.theNumber + 1;
+    }
+    if (this.theNumber > 20) {
+      //this.theText.setText("Do you notice how slow you move and \n how hard it is for you to jump? \n It's because your so fat... \n Waddle on over to you're friend Ian. ");
+    }
+    if (this.moveLeft) {
+      this.player.setVelocityX(25);
+      this.theNumber = this.theNumber + 100;
+      this.player.anims.play('right', true);
+      this.player.flipX = true;
+      this.screenTouch = true;
+      this.check = true;
+    } else if (!this.moveLeft && this.check === true) {
+      this.player.setVelocityX(0);
+      this.player.anims.play('turn');
+    }
+  }
+}
